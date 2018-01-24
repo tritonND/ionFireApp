27d643c4aba82263ab8b6f77ea8c0766e0dd90fd
Ndukwe Onugu <trittons2003@hotmail.com>
2018-01-24 13:44:04 +0000

    publish this first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,42 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { WelcomePage } from '../pages/welcome/welcome';
+import { FormsPage } from '../pages/forms/forms';
+import { LoginPage } from '../pages/login/login';
+//import firebase from 'firebase';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBjeEG0luJ-J6elIGEg3Oem_rm0k5KuhI8",
+  authDomain: "fireapp2-9ba73.firebaseapp.com",
+  databaseURL: "https://fireapp2-9ba73.firebaseio.com",
+  projectId: "fireapp2-9ba73",
+  storageBucket: "",
+  messagingSenderId: "117392197923"
+ };
+
+ 
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage, WelcomePage, FormsPage, LoginPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage, WelcomePage, FormsPage, LoginPage
   ],
   providers: [
     StatusBar,
